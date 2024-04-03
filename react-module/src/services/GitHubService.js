377c82afcb2eea8cf08fee02277c4850c32a1359
Himanshu Rai <swavyast@gmail.com>
2024-04-03 15:45:06 +0530

    commit - 000000000000000000008
    Signed-off-by: Himanshu Rai <swavyast@gmail.com>

--- react-module/src/services/GitHubService.js
+++ react-module/src/services/GitHubService.js
@@ -16,6 +16,12 @@ class GitHubService {
             .then( ( res ) => res.data )
             .catch( ( error ) => { throw error; } );
     }
+    getRepositoryByName (name) {
+        return axios.get( '{GIT_API_BASE_URL}/${name}' )
+            .then( ( res ) => res.data )
+            .catch( ( error ) => { throw error; } );
+    }
+
 }
 
 export default new GitHubService();
